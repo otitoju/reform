200eeb42863c1e4daffb2139ae7b84217bdce727
otitoju <otitojuoluwapelumi@gmail.com>
2018-08-17 01:24:29 -0700

    refactor views

---
+++ client-view/myrecipe/public/boot/js/jquery.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#send").click(function () {
+          var error0 = false;
+              $('#in').show(500);
+              $('#in').delay(2000);
+              $('#in').animate({
+                  height: 'toggle'
+              }, 500, function () {
+                  // Animation complete.
+              });
+              error0 = true; // change the error state to true  
+        })
+      })
