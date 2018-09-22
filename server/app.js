f5003ffd8ad519d7a316e736568dec0024e99d90
otitoju <otitojuoluwapelumi@gmail.com>
2018-09-22 01:58:32 -0700

    disable cors

--- server/app.js
+++ server/app.js
@@ -6,9 +6,9 @@ const routes = require('./routes/routes')
 const path = require('path')
 const db = require('./config/dbconfig')
 const env = require('dotenv').config()
-const cors = require('cors')
+//const cors = require('cors')
 
-app.use(cors())
+//app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
